3e1e59ceeab51007f5672a2e9b779f94d22aadbd
Mauriciojedi <mauricioempisani@gmail.com>
2020-11-25 19:47:50 -0300

    versao002

--- model/Medico.php
+++ model/Medico.php
@@ -1,8 +1,66 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Medico{
+private $id;
+private $email;
+private $nome;
+private $senha;
+private $dt_criacao;
+private $dt_alteracao;
 
+public function __construct() {
+
+}
+
+function getId() {
+    return $this->id;
+}
+
+function getEmail() {
+    return $this->email;
+}
+
+function getNome() {
+    return $this->nome;
+}
+
+function getSenha() {
+    return $this->senha;
+}
+
+function getDt_criacao() {
+    return $this->dt_criacao;
+}
+
+function getDt_alteracao() {
+    return $this->dt_alteracao;
+}
+
+function setId($id) {
+    $this->id = $id;
+}
+
+function setEmail($email) {
+    $this->email = $email;
+}
+
+function setNome($nome) {
+    $this->nome = $nome;
+}
+
+function setSenha($senha) {
+    $this->senha = $senha;
+}
+
+function setDt_criacao($dt_criacao) {
+    $this->dt_criacao = $dt_criacao;
+}
+
+function setDt_alteracao($dt_alteracao) {
+    $this->dt_alteracao = $dt_alteracao;
+}
+
+
+
+}
+;
--- model/MedicoADO.php
+++ model/MedicoADO.php
@@ -1,8 +1,14 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'Medico.php';
+class MedicoADO extends Medico{
+    
+    protected function cadastrar(Medico $m){
+        $m1=new Medico();
+        $m1->setNome($m->getNome());
+        $m1->setEmail($m->getEmail());
+        $m1->setSenha($m->getSenha());
+        $m1->setDt_criacao(date('d/m/Y H:i'));
+        $m1->setDt_alteracao(date('d/m/Y H:i'));
+    }
+}
 
